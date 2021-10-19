71c79caee0cb6ba4bc79de8c0029c8efd208377d
Henri-Simon <etu40354@henallux.be>
2021-10-19 16:11:46 +0200

    Bit plaintext converted in block of 128 and padding for the first block

--- cbc_ctr_aes-256.py
+++ cbc_ctr_aes-256.py
@@ -1,15 +1,18 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-key = get_random_bytes(32)
-cipher = AES.new(key, AES.MODE_ECB)
-plaintext = "Very cool and very long plaintext to be encrypted, wow such a cool plaintext"
+#convert the bit plaintext into block of 128, the function take in argument the bitstream reversed.
+#so i can add padding to the begining more easily
+def convert_block_of_128_bit(bit_plaintext):
+    blocks = [bit_plaintext[i:i+128] for i in range(0, len(bit_plaintext), 128)]
+    blocks.reverse()                    
+    blocks_inverse = []
+    for b in range(len(blocks)):
+        blocks_inverse.append(format(int(blocks[b][::-1],2), '0128b'))
+    
+    return blocks_inverse
 
-bit_plaintext = ''.join(format(ord(c), 'b') for c in plaintext)
-print(bit_plaintext)
-print(len(bit_plaintext))
-
-def encrypt_with_cbc(key, plain_text):
+def encrypt_with_cbc(key, bit_plaintext):
     pass
 
 def decrypt_with_cbc(key, cipher_text):
@@ -20,3 +23,16 @@ def encrypt_with_ctr(key, plaintext):
 
 def decrypt_with_ctr(key, ciphertext):
     pass
+
+key = get_random_bytes(32)
+cipher = AES.new(key, AES.MODE_ECB)
+plaintext = "Very cool and very long plaintext to be encrypted, wow such a cool plaintext"
+
+bit_plaintext = ''.join(format(ord(c), 'b') for c in plaintext)
+print(bit_plaintext)
+print("----------------------")
+
+b = convert_block_of_128_bit(bit_plaintext[::-1])
+
+for i in range(len(b)):
+    print(b[i])
