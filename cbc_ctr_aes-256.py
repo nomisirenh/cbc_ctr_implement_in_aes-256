cf5a282861bf61a45a84721829acd20171c98d12
Henri-Simon <etu40354@henallux.be>
2021-10-20 21:25:25 +0200

    Encrypt CTR work and match with true CTR

--- cbc_ctr_aes-256.py
+++ cbc_ctr_aes-256.py
@@ -1,6 +1,9 @@
+from typing import Counter
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
+from Crypto.Util.number import bytes_to_long
+from Crypto.Util import Counter
 
 #convert the bit plaintext into block of 128, the function take in argument the bitstream reversed.
 #so i can add padding to the begining more easily
@@ -42,17 +45,24 @@ def encrypt_with_ctr(plaintext, cipher, iv):
     blocks = convert_block_of_128_bit(plaintext)
     cipher_text = []
 
-    c_iv = iv
     for i in range(len(blocks)):
-        c = cipher.encrypt(c_iv)
+        if i == 0:
+            c = cipher.encrypt(iv)
+        else:
+            c = cipher.encrypt((int.from_bytes(iv, 'big') + i).to_bytes(16, 'big'))
+
         xor = byte_xor(blocks[i], c)
         cipher_text.append(xor)
-        c_iv += i.to_bytes(16,'big')
     
     return cipher_text
 
-def decrypt_with_ctr(key, ciphertext):
-    pass
+def decrypt_with_ctr(cipher_text, cipher, iv):
+    #blocks = convert_block_of_128_bit(cipher_text)
+    blocks = cipher_text
+    decrypt_text = []
+
+    for i in range(len(blocks)):
+        pass
 
 def main():
 
@@ -60,16 +70,16 @@ def main():
     key = b'\xc9s\x89@\t\x0f\xdd\x9b\x88\xbd\x9b\xa8\xd9\x99\x8a\x87(\xf1e]5\xdbj\xe6\n\xf1!e\x9f\x06\xb8\xe5'
     cipher = AES.new(key, AES.MODE_ECB)
     
-
     plaintext = "Very cool and very long plaintext to be encrypted, wow such a cool plaintext".encode("UTF-8")
     plaintext = pad(plaintext, 16)
 
     #iv = get_random_bytes(16)
     iv = b'>Z\x861\x18\x9ek#\xe9\xda:y\x0cu,\x1b'
 
-    cipher2 = AES.new(key, AES.MODE_CBC, iv)
-    #cipher3 = AES.new(key, AES.MODE_CTR, iv)
-    test = cipher2.encrypt(plaintext)
+    counter = Counter.new(128,initial_value = bytes_to_long(iv))
+    true_cipher2 = AES.new(key, AES.MODE_CBC, iv)
+    true_cipher3 = AES.new(key, AES.MODE_CTR, counter=counter)
+    test = true_cipher3.encrypt(plaintext)
     print(test)
     print(" ")
 
