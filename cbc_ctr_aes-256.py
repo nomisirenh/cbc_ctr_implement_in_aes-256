0ad542953e27af833d6981c55a2a1b0ed69e8c91
Henri-Simon <etu40354@henallux.be>
2021-10-20 23:11:01 +0200

    Enrypt return one block and decrypt one string

--- cbc_ctr_aes-256.py
+++ cbc_ctr_aes-256.py
@@ -1,7 +1,7 @@
 from typing import Counter
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
 from Crypto.Util.number import bytes_to_long
 from Crypto.Util import Counter
 
@@ -17,33 +17,33 @@ def byte_xor(byte1, byte2):
 
 def encrypt_with_cbc(plaintext, cipher, iv):
     blocks = convert_block_of_128_bit(plaintext)
-    cipher_text = []
+    cipher_text = bytes()
 
     c = iv
     for i in range(len(blocks)):
         xor = byte_xor(blocks[i], c)
         c = cipher.encrypt(xor)
-        cipher_text.append(c)
+        cipher_text += c
 
     return cipher_text
 
 def decrypt_with_cbc(cipher_text, cipher, iv):
-    #blocks = convert_block_of_128_bit(cipher_text)
-    blocks = cipher_text
-    decrypt_text = []
+    blocks = convert_block_of_128_bit(cipher_text)
+    #blocks = cipher_text
+    decrypt_text = bytes()
 
     b = iv
     for i in range(len(blocks)):
         d = cipher.decrypt(blocks[i])
         xor = byte_xor(d, b)
         b = blocks[i]
-        decrypt_text.append(xor)
+        decrypt_text += xor
 
-    return decrypt_text  
+    return unpad(decrypt_text, 16).decode('utf-8')  
 
 def encrypt_with_ctr(plaintext, cipher, iv):
     blocks = convert_block_of_128_bit(plaintext)
-    cipher_text = []
+    cipher_text = bytes()
 
     for i in range(len(blocks)):
         if i == 0:
@@ -52,43 +52,53 @@ def encrypt_with_ctr(plaintext, cipher, iv):
             c = cipher.encrypt((int.from_bytes(iv, 'big') + i).to_bytes(16, 'big'))
 
         xor = byte_xor(blocks[i], c)
-        cipher_text.append(xor)
+        cipher_text += xor
     
     return cipher_text
 
 def decrypt_with_ctr(cipher_text, cipher, iv):
-    #blocks = convert_block_of_128_bit(cipher_text)
-    blocks = cipher_text
-    decrypt_text = []
+    blocks = convert_block_of_128_bit(cipher_text)
+    #blocks = cipher_text
+    decrypt_text = bytes()
 
     for i in range(len(blocks)):
-        pass
+        if i == 0:
+            c = cipher.encrypt(iv)
+        else:
+            c = cipher.encrypt((int.from_bytes(iv, 'big') + i).to_bytes(16, 'big'))
+
+        xor = byte_xor(blocks[i], c)
+        decrypt_text += xor
+    
+    return unpad(decrypt_text, 16).decode('utf-8')
 
 def main():
 
     #key = get_random_bytes(32)
     key = b'\xc9s\x89@\t\x0f\xdd\x9b\x88\xbd\x9b\xa8\xd9\x99\x8a\x87(\xf1e]5\xdbj\xe6\n\xf1!e\x9f\x06\xb8\xe5'
-    cipher = AES.new(key, AES.MODE_ECB)
     
     plaintext = "Very cool and very long plaintext to be encrypted, wow such a cool plaintext".encode("UTF-8")
     plaintext = pad(plaintext, 16)
 
     #iv = get_random_bytes(16)
     iv = b'>Z\x861\x18\x9ek#\xe9\xda:y\x0cu,\x1b'
+    
+    cipher = AES.new(key, AES.MODE_ECB)
 
+    #Cipher from Crypto library
     counter = Counter.new(128,initial_value = bytes_to_long(iv))
     true_cipher2 = AES.new(key, AES.MODE_CBC, iv)
     true_cipher3 = AES.new(key, AES.MODE_CTR, counter=counter)
-    test = true_cipher3.encrypt(plaintext)
-    print(test)
-    print(" ")
+    #test = true_cipher3.encrypt(plaintext)
 
-    c_text = encrypt_with_cbc(plaintext, cipher, iv)
-    d_text = decrypt_with_cbc(c_text, cipher, iv)
+    #Encrypt
+    c_text_cbc = encrypt_with_cbc(plaintext, cipher, iv)
+    c_text_ctr = encrypt_with_ctr(plaintext, cipher, iv)
 
-    c_text = encrypt_with_ctr(plaintext, cipher, iv)
-    print(" ")
-    for c in c_text:
-        print(c)
+    #Decrypt
+    d_text_cbc = decrypt_with_cbc(c_text_cbc, cipher, iv)
+    d_text_ctr = decrypt_with_ctr(c_text_ctr, cipher, iv)
+
+    print(d_text_ctr)
 
 main()
